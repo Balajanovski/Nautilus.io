fc4d16342aa8e837fe6bfbc9d406be455764a3d5
James Balajan <jbalajanbusiness@gmail.com>
2016-12-19 10:25:15 +1100

    Fixed script

--- scripts/word-count.js
+++ scripts/word-count.js
@@ -5,14 +5,14 @@
 $(document).ready(function(){
     $("#message").keydown(function(){
         var max_length = 500 - $("#message").val().length;
-        if (max_length == NaN)
+        if (max_length === NaN)
             $("#word-count").text(max_length + " characters left");
         else
             $("#word-count").text(max_length + " characters left");
     });
     $("#message").keyup(function(){
         var max_length = 500 - $("#message").val().length;
-        if (max_length == NaN)
+        if (max_length === NaN)
             $("#word-count").text(max_length + " characters left");
         else
             $("#word-count").text(max_length + " characters left");
