422d6515f1258545da6183331121b2750b0479b3
ZaneHannanAU <ZaneHannanAU@users.noreply.github.com>
2017-01-03 20:49:59 +1100

    Removed a jQuery requirement.

--- scripts/word-count.js
+++ scripts/word-count.js
@@ -1,3 +1,4 @@
+/**/
 $(document).ready(function(){
     $("#message").keydown(function(){
         var max_length = 500 - $("#message").val().length;
@@ -6,7 +7,7 @@ $(document).ready(function(){
         else
             $("#word-count").text(max_length + " characters left");
     });
-    $("#message").keyup(function(){
+   $("#message").keyup(function(){
         var max_length = 500 - $("#message").val().length;
         if (max_length === NaN)
             $("#word-count").text(max_length + " characters left");
@@ -14,3 +15,19 @@ $(document).ready(function(){
             $("#word-count").text(max_length + " characters left");
     });
 });
+/**/
+/* i'll fix later /*
+const message = document.querySelector('#message')
+const messageMax = parseInt(message.getAttribute('maxlength'))
+var counter = document.querySelector('#word-count').textContent
+var countWord = function(event){
+	var messageTemp = document.querySelector('#message')
+	if (messageTemp.value.length <= messageMax) {
+		counter = (messageMax - messageTemp.value.length) + ' characters available'
+	} else {
+		counter = (messageTemp.value.length - messageMax) + ' over the limit!'
+	}
+}
+message.addEventListener('input', countWord)
+message.addEventListener('keyup', countWord)
+/**/
